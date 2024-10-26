93b8e6e928b203f4359c6281c198ccfd72777aae
Leslie Martin <47051873+llamartin@users.noreply.github.com>
2024-10-26 16:21:58 +1100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const researchers = [
+    {
+        name: "A/Prof Leslie Martin",
+        title: "Associate Professor",
+        photo: "images/leslie-martin.jpg",  // Update with actual image path
+        website: "https://findanexpert.unimelb.edu.au/profile/265067-leslie-martin",
+        isPrincipalInvestigator: true
+    },
+    {
+        name: "Prof David Byrne",
+        title: "Professor",
+        photo: "images/david-byrne.jpg",    // Update with actual image path
+        website: "https://findanexpert.unimelb.edu.au/profile/709853-david-byrne"
+    },
+    // ... other researchers
+];
+
+// All the JavaScript functions from the previous HTML file
+// ... Copy all the JavaScript from between the <script> tags ...
